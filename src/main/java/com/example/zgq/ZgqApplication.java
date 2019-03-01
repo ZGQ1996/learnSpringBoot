e3992d7b04b58875b1295c39be4c5977efdf222e
静尽净 <1226807499@qq.com>
2019-03-02 00:00:19 +0800

    项目添加filterc测试

--- src/main/java/com/example/zgq/ZgqApplication.java
+++ src/main/java/com/example/zgq/ZgqApplication.java
@@ -2,6 +2,7 @@ package com.example.zgq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -9,6 +10,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @ImportResource(locations = {"classpath:spring-mvc.xml"})
 //加上这个注解，swagger在浏览器就可以访问了，不然就404
 @EnableSwagger2
+//加上这个注解，为了filter过滤器
+@ServletComponentScan
 public class ZgqApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/zgq/filter/test/MyFilter.java
@@ -0,0 +1,32 @@
+package com.example.zgq.filter.test;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * @ Author     ：Zgq
+ * @ Date       ：Created in 23:26 2019/3/1
+ * @ Description：过滤器
+ * @ Modified By：
+ * @Version: $
+ */
+
+@WebFilter(filterName = "myfilter",urlPatterns = "/*")
+public class MyFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.printf("过滤器执行");
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.printf("过滤器初始化");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.printf("过滤器销毁");
+    }
+}
